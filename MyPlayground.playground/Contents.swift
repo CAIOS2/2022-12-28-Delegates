443b89ec177401ed58621e691a4a4be770a49d55
tadaspetrikas <67460225+tadaspetrikas@users.noreply.github.com>
2022-12-28 18:42:52 +0200

    Add 2nd excercise

--- MyPlayground.playground/Contents.swift
+++ MyPlayground.playground/Contents.swift
@@ -1,31 +1,61 @@
+
 protocol FoodReadyDelegate: AnyObject {
     func didFinishMakingFood()
+    func didFinishMakingFood(restaurantName: String)
 }
 
-class Restaurant {
+class Restaurant { //✅
     weak var delegate: FoodReadyDelegate?
+    let name: String //✅
+    
+    init(name: String) {
+        self.name = name
+    }
     
     func makeFood() {
         print("Making food")
-        delegate?.didFinishMakingFood()
+        delegate?.didFinishMakingFood(restaurantName: name)
     }
 }
 
 
 class FoodOrderer: FoodReadyDelegate {
     
+    let restaurants: [Restaurant]
+    
+    init(restaurants: [Restaurant]) {
+        self.restaurants = restaurants
+    }
+    
     func didFinishMakingFood() {
         print("Food is ready")
     }
     
-    func buyFood() {
-        print("Client order food")
-        let restaurant = Restaurant()
-        restaurant.delegate = self
-        restaurant.makeFood()
+    func didFinishMakingFood(restaurantName: String) {
+        print("Food is ready in \(restaurantName)")
     }
     
+//    func buyFood() {
+//        print("Client order food")
+//        let restaurant = Restaurant(name: "Kakes makes blynai")
+//        restaurant.delegate = self
+//        restaurant.makeFood()
+//    }
+    
+    func orderFood() {
+        for restaurant in restaurants {
+            restaurant.delegate = self
+            restaurant.makeFood()
+        }
+    }
 }
 
-let foodOrderer = FoodOrderer()
-foodOrderer.buyFood()
+let restaurants = [
+    Restaurant(name: "Kakes makes blynai"),
+    Restaurant(name: "Gruzinu kacepuriai"),
+    Restaurant(name: "Suppa dupa wrape doner kebabai")
+]
+
+let foodOrderer = FoodOrderer(restaurants: restaurants)
+foodOrderer.orderFood()
+//foodOrderer.buyFood()
